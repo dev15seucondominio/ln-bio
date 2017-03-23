23f52dfb81fa5ac64db519cc95dccfdf5c18dbd3
dev15seucondominio <dev15.seucondominio@gmail.com>
2017-03-23 12:49:17 -0300

    Creando repositoria para a primeira versão do Coletor Biometrico LN-BIO

---
+++ Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Runtime.Serialization;
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string path = "finger.bmp";
+        string encode;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] pName = new byte[8 * 128];
+            int i, iNum;
+
+            //comboBox1.Items.Clear();
+            iNum = Win32.AvzFindDevice(pName);
+
+            for (i = 0; i < iNum; i++)
+            {
+                string encode = Encoding.Default.GetString(pName, i * 128, 128);
+            }
+
+            long lRet;
+
+            lRet = Win32.AvzOpenDevice(Convert.ToInt16(encode), 0);
+
+            if (lRet == 0)
+            {
+                MessageBox.Show("Conectado");
+                UInt16 uStatus = 0;
+                UInt16 Ret;
+                MessageBox.Show("Coloque o Dedo");
+                while (uStatus == 0)
+                {
+                    Win32.AvzGetImage(Convert.ToInt16(0), Win32.gpImage, ref uStatus);
+                    Win32.AvzSaveHueBMPFile(path, Win32.gpImage);
+
+                    Image bitmap1 = Image.FromFile(path);
+                    System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                    bitmap1.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+                    bitmap1.Dispose();
+                    Image bitmap2 = Image.FromStream(ms);
+
+                    pictureBox1.Image = bitmap2;
+                
+                    ms.Close();
+                    Win32.AvzProcess(Win32.gpImage, Win32.gpFeatureA, Win32.gpBin, 1, 1, 94);
+
+                }
+
+                while (uStatus == 1)
+                {
+                    MessageBox.Show("Retire o Dedo");
+                    Win32.AvzGetImage(Convert.ToInt16(0), Win32.gpImage, ref uStatus);
+                }
+
+                MessageBox.Show("Coloque o Mesmo Dedo Novamente");
+                while (uStatus == 0)
+                {
+                    Win32.AvzGetImage(Convert.ToInt16(0), Win32.gpImage, ref uStatus);
+                    Win32.AvzSaveHueBMPFile(path, Win32.gpImage);
+
+                    Image bitmap1 = Image.FromFile(path);
+                    System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                    bitmap1.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+                    bitmap1.Dispose();
+                    Image bitmap2 = Image.FromStream(ms);
+
+                    pictureBox1.Image = bitmap2;
+
+                    ms.Close();
+                
+                    Win32.AvzProcess(Win32.gpImage, Win32.gpFeatureB, Win32.gpBin, 1, 1, 94);
+                }
+
+                Ret = Win32.AvzMatch(Win32.gpFeatureA, Win32.gpFeatureB, 9, 60);
+                Win32.AvzCloseLED(Convert.ToInt16(encode));
+
+                if (Ret == 0)
+                {
+                    MessageBox.Show("Sucesso na Captura");
+                    Ret = Win32.AvzPackFeature(Win32.gpFeatureA, Win32.gpFeatureB, Win32.gpFeatureBuf1);
+                    using (BinaryWriter binWriter = new BinaryWriter(File.Open("MyFile.anv", FileMode.Create)))
+                    {
+                        binWriter.Write(Win32.gpFeatureBuf1, 0, Ret);
+                    }
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Erro na Captura");
+                }
+            }
+            else
+            {
+                //comboBox1.Items.Clear();
+                Win32.AvzCloseLED(Convert.ToInt16(encode));
+                MessageBox.Show("ERROR - Desconectado");
+            }
+
+            Win32.AvzCloseDevice(Convert.ToInt16(encode));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Win32.AvzCloseLED(Convert.ToInt16(encode));
+
+            byte[] pName = new byte[8 * 128];
+            int i, iNum;
+
+            //comboBox1.Items.Clear();
+            iNum = Win32.AvzFindDevice(pName);
+
+            for (i = 0; i < iNum; i++)
+            {
+                string encode = Encoding.Default.GetString(pName, i * 128, 128);
+            }
+
+            Win32.AvzCloseDevice(Convert.ToInt16(encode));
+            Win32.AvzCloseLED(Convert.ToInt16(encode));
+            MessageBox.Show("ERROR - Desconectado");
+        }
+        
+        private WebRequest request;
+        private Stream dataStream;
+
+        private string status;
+
+        public String Status
+        {
+            get
+            {
+                return status;
+            }
+            set
+            {
+                status = value;
+            }
+        }
+
+        public void MyWebRequest(string url)
+        {
+            // Create a request using a URL that can receive a post.
+
+            request = WebRequest.Create(url);
+        }
+
+        public byte[] FileToByteArray(string fileName)
+        {
+            byte[] buff = null;
+            FileStream fs = new FileStream(fileName,
+                                       FileMode.Open,
+                                       FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            long numBytes = new FileInfo(fileName).Length;
+            buff = br.ReadBytes((int)numBytes);
+            return buff;
+        }
+
+        public void MyWebRequest(string url, string data)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.Accept = "Accept=text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            request.Headers["cliente"] = textBox1.Text;
+            request.Headers["equipamento"] = textBox2.Text;
+            request.Headers["usuario"] = textBox3.Text;
+            request.Headers["digital"] = data;
+            
+            byte[] _byteVersion = Encoding.ASCII.GetBytes(string.Concat("equipamento=", data));
+
+            request.ContentLength = _byteVersion.Length;
+
+            Stream stream = request.GetRequestStream();
+            stream.Write(_byteVersion, 0, _byteVersion.Length);
+            stream.Close();
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                Console.WriteLine(reader.ReadToEnd());
+                Console.WriteLine(request);
+                Console.WriteLine(stream);
+                Console.WriteLine(_byteVersion);
+                foreach(var i in _byteVersion)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine(_byteVersion.Length);
+            }
+        }
+
+
+        public void MyWebRequest(string url, string method, string data)
+            
+        {
+            request = WebRequest.Create(url);
+
+            // Create POST data and convert it to a byte array.
+            string postData = data;
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+            // Set the ContentType property of the WebRequest.
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            // Set the ContentLength property of the WebRequest.
+            request.ContentLength = byteArray.Length;
+
+            // Get the request stream.
+            Stream dataStream = request.GetRequestStream();
+
+            // Write the data to the request stream.
+            dataStream.Write(byteArray, 0, byteArray.Length);
+
+            // Close the Stream object.
+            dataStream.Close();
+
+        }
+
+        public string GetResponse()
+        {
+            // Get the original response.
+            WebResponse response = request.GetResponse();
+
+            this.Status = ((HttpWebResponse)response).StatusDescription;
+
+            // Get the stream containing all content returned by the requested server.
+            dataStream = response.GetResponseStream();
+
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+
+            // Read the content fully up to the end.
+            string responseFromServer = reader.ReadToEnd();
+
+            // Clean up the streams.
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            return responseFromServer;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Enviar Digital");
+
+            UInt16 Ret = 0;
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Feature files|*.anv";
+            DialogResult result;
+
+            // Displays the MessageBox.
+
+            result = dlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                byte[] bbyt = FileToByteArray(dlg.FileName);
+
+                string buffer = "";
+                foreach (var u in bbyt) buffer += Convert.ToString(u, 16) + ",";
+
+                Console.Write(buffer);
+                MyWebRequest("http://seucondominio.ddns.net:3001/guaritas/buffer_digital", buffer);
+            }
+        }
+
+        private void init(object sender, EventArgs e)
+        {
+            //char[,] pName = new char[8, 128];
+            byte[] pName = new byte[8 * 128];
+            int i, iNum;
+
+            //comboBox1.Items.Clear();
+            iNum = Win32.AvzFindDevice(pName);
+
+            for (i = 0; i < iNum; i++)
+            {
+                encode = Encoding.Default.GetString(pName, i * 128, 128);
+            }
+
+            //if (iNum > 0) comboBox1.SelectedIndex = 0;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Win32.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApp1
+{
+    class Win32
+    {
+        public static byte[] gpImage = new byte[256 * 296];
+        public static byte[] gpBin = new byte[256 * 296];
+        public static byte[] gpFeature = new byte[256];
+        public static byte[] gpFeatureA = new byte[256];
+        public static byte[] gpFeatureB = new byte[256];
+        public static byte[] gpFeatureBuf1 = new byte[512];
+        public static byte[] gpFeatureBuf2 = new byte[512];
+        public static byte[] gpFeatureLib1 = new byte[10000 * 256];
+        public static byte[] gpFeatureLib2 = new byte[10000 * 256];
+        [DllImport("AvzScanner.dll")]
+        public static extern UInt16 AvzFindDevice(byte[] pDeviceName);
+        [DllImport("AvzScanner.dll")]
+        public static extern UInt32 AvzOpenDevice(Int16 uDeviceID, UInt32 hWnd);
+        [DllImport("AvzScanner.dll")]
+        public static extern UInt32 AvzCloseDevice(Int16 uDeviceID);
+        [DllImport("AvzScanner.dll")]
+        public static extern UInt32 AvzGetID(Int16 wDeviceID, byte[] pID);
+        [DllImport("AvzScanner.dll")]
+        public static extern void AvzGetImage(Int16 wDeviceID, byte[] pImage, ref UInt16 uStatus);
+        [DllImport("AvzScanner.dll")]
+        public static extern void AvzGetImage(Int16 wDeviceID, IntPtr pImage, ref UInt16 uStatus);
+        [DllImport("AvzScanner.dll")]
+        public static extern UInt32 AvzProcess(byte[] pimagein, byte[] feature, byte[] pimagebin, byte bthin, byte bdrawfea, UInt16 comst);
+        [DllImport("AvzScanner.dll")]
+        public static extern UInt32 AvzProcess(IntPtr ptr, byte[] feature, byte[] pimagebin, byte bthin, byte bdrawfea);
+        [DllImport("AvzScanner.dll")]
+        public static extern UInt16 AvzSaveHueBMPFile([MarshalAs(UnmanagedType.LPStr)]String pName, byte[] praw);
+        [DllImport("AvzScanner.dll")]
+        public static extern UInt16 AvzSaveClrBMPFile([MarshalAs(UnmanagedType.LPStr)]String pName, byte[] praw);
+        [DllImport("AvzScanner.dll")]
+        public static extern UInt16 AvzShowImage(System.IntPtr pHND, byte[] praw, UInt32 a, UInt32 b, UInt32 c, UInt32 d, UInt32 e, UInt32 f, UInt32 g, UInt32 h);
+        [DllImport("AvzScanner.dll")]
+        public static extern UInt16 AvzPackFeature(byte[] pFeature1, byte[] pFeature2, byte[] pFeatureBuf);
+        [DllImport("AvzScanner.dll")]
+        public static extern UInt16 AvzUnpackFeature(byte[] pFeatureBuf, [Out] byte[] pFeature1, [Out] byte[] pFeature2);
+        [DllImport("AvzScanner.dll")]
+        public static extern UInt16 AvzMatch(byte[] pFeature1, byte[] pFeature2, UInt16 level, UInt16 rotate);
+        [DllImport("AvzScanner.dll")]
+        public static extern Int16 AvzMatchN(byte[] pFeature, byte[] gpFeatureLib, UInt32 FingerNum, UInt16 level, UInt16 rotate);
+        [DllImport("AvzScanner.dll")]
+        public static extern Int16 AvzCloseLED(Int16 uDeviceID);
+        [DllImport("AvzScanner.dll")]
+        public static extern Int16 AvzOpenLED(Int16 uDeviceID);
+    }
+}
+
